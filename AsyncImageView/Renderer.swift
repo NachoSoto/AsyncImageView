a2ca93cfd1aabbb32d1c8b9a66787d4720a4b0a4
NachoSoto <ignaciosoto90@gmail.com>
2015-11-22 23:37:37 +0000

    RendererType now produces a SignalProducer. Added SynchronousRendererType.

--- AsyncImageView/Renderer.swift
+++ AsyncImageView/Renderer.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 /// Information required to produce an image
 public protocol RenderDataType: Hashable {
 	var size: CGSize { get }
 }
 
-public protocol RendererType {
-	typealias RenderData: RenderDataType
-
-	func renderImageWithData(data: RenderData) -> UIImage
-}
-
 public struct RenderResult {
 	let image: UIImage
 	let cacheHit: Bool
+}
+
+public protocol RendererType {
+	typealias RenderData: RenderDataType
+
+	func renderImageWithData(data: RenderData) -> SignalProducer<UIImage, NoError>
+}
+
+public protocol SynchronousRendererType {
+	typealias RenderData: RenderDataType
+
+	func renderImageWithData(data: RenderData) -> UIImage
 }

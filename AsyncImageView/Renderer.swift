ffc74af801d8fc1494e2c25aa5f983347b14febb
NachoSoto <ignaciosoto90@gmail.com>
2015-11-22 23:38:02 +0000

    Created AnyRenderer to decorate RendererType

--- AsyncImageView/Renderer.swift
+++ AsyncImageView/Renderer.swift
@@ -30,3 +30,43 @@ public protocol SynchronousRendererType {
 
 	func renderImageWithData(data: RenderData) -> UIImage
 }
+
+/// A type-erased `RendererType`.
+public final class AnyRenderer<T: RenderDataType>: RendererType {
+	private let renderBlock: (T) -> SignalProducer<UIImage, NoError>
+
+	/// Constructs an `AnyRenderer` with a `SynchronousRendererType`.
+	/// The created `SignalProducer` will simply emit the result 
+	/// of `renderImageWithData`.
+	public convenience init<R: SynchronousRendererType where R.RenderData == T>(renderer: R) {
+		self.init { data in
+			return SignalProducer { observer, disposable in
+				if !disposable.disposed {
+					observer.sendNext(renderer.renderImageWithData(data))
+					observer.sendCompleted()
+				} else {
+					observer.sendInterrupted()
+				}
+			}
+		}
+	}
+
+	/// Creates an `AnyRenderer` based on another `RendererType`.
+	public convenience init<R: RendererType where R.RenderData == T>(renderer: R) {
+		self.init(renderBlock: renderer.renderImageWithData)
+	}
+
+	private init(renderBlock: (T) -> SignalProducer<UIImage, NoError>) {
+		self.renderBlock = renderBlock
+	}
+
+	public func renderImageWithData(data: T) -> SignalProducer<UIImage, NoError> {
+		return self.renderBlock(data)
+	}
+}
+
+extension SynchronousRendererType {
+	public var asyncRenderer: AnyRenderer<Self.RenderData> {
+		return AnyRenderer(renderer: self)
+	}
+}

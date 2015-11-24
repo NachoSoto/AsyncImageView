a7acac5a485eaf35ee1d2d5e172969a19112eefa
NachoSoto <ignaciosoto90@gmail.com>
2015-11-24 14:48:54 -0800

    Added tests for InMemoryCache (and any CacheType): #3

---
+++ AsyncImageViewTests/CachingSpec.swift
@@ -0,0 +1,47 @@
+//
+//  CachingSpec.swift
+//  AsyncImageView
+//
+//  Created by Nacho Soto on 11/24/15.
+//  Copyright © 2015 Nacho Soto. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+import RandomKit
+
+@testable import AsyncImageView
+
+private func testCache<T: CacheType where T.Value: Equatable>(cacheCreator cacheCreator: () -> T, keyCreator: () -> T.Key, valueCreator: () -> T.Value) {
+	var cache: T!
+
+	beforeEach {
+		cache = cacheCreator()
+	}
+
+	it("returns nil when not cached") {
+		expect(cache.valueForKey(keyCreator())).to(beNil())
+	}
+
+	it("recovers value after saving it") {
+		let key = keyCreator()
+		let value = valueCreator()
+
+		cache.setValue(value, forKey: key)
+
+		expect(cache.valueForKey(key)) == value
+	}
+}
+
+class InMemoryCacheSpec: QuickSpec {
+	override func spec() {
+		describe("InMemoryCache") {
+			testCache(
+				cacheCreator: { InMemoryCache(cacheName: "test") },
+				keyCreator: String.random,
+				valueCreator: String.random
+			)
+		}
+	}
+}

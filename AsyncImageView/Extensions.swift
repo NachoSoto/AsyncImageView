7656121000f3a0b922529cece6e3ee30088050b0
NachoSoto <ignaciosoto90@gmail.com>
2015-11-24 15:42:51 -0800

    UIImage implements NSDataConvertible

---
+++ AsyncImageView/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  AsyncImageView
+//
+//  Created by Nacho Soto on 11/24/15.
+//  Copyright © 2015 Nacho Soto. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage: NSDataConvertible {
+	// init(data:) is already implemented by UIImage.
+
+	public var data: NSData? {
+		return UIImageJPEGRepresentation(self, 1)
+	}
+}

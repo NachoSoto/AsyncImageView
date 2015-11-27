8d64f2a6ba82ca4c85ba30f388be1527e82a3079
NachoSoto <ignaciosoto90@gmail.com>
2015-11-27 14:47:03 -0800

    Created ContextRenderer. Fixes #10

---
+++ AsyncImageView/Renderers/ContextRenderer.swift
@@ -0,0 +1,40 @@
+//
+//  ContextRenderer.swift
+//  AsyncImageView
+//
+//  Created by Nacho Soto on 11/27/15.
+//  Copyright © 2015 Nacho Soto. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+import ReactiveCocoa
+
+public typealias ContextRenderingBlock = (CGContextRef) -> ()
+
+/// `SynchronousRendererType` which generates a `UIImage` by rendering into a context.
+public final class ContextRenderer<Data: RenderDataType>: SynchronousRendererType {
+	private let scale: CGFloat
+	private let opaque: Bool
+	private let renderingBlock: ContextRenderingBlock
+
+	/// - opaque: A Boolean flag indicating whether the bitmap is opaque. 
+	/// If you know the bitmap is fully opaque, specify YES to ignore the 
+	/// alpha channel and optimize the bitmap’s storage.
+	public init(scale: CGFloat, opaque: Bool, renderingBlock: ContextRenderingBlock) {
+		self.scale = scale
+		self.opaque = opaque
+		self.renderingBlock = renderingBlock
+	}
+
+	public func renderImageWithData(data: Data) -> UIImage {
+		UIGraphicsBeginImageContextWithOptions(data.size, self.opaque, self.scale)
+
+		self.renderingBlock(UIGraphicsGetCurrentContext()!)
+
+		let image = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+
+		return image
+	}
+}

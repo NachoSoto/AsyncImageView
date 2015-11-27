9105c559d3a035df887109bd2a249341ba5d523a
NachoSoto <ignaciosoto90@gmail.com>
2015-11-27 13:40:55 -0800

    Created LocalImageRenderer. Fixes #18.

---
+++ AsyncImageView/Renderers/LocalImageRenderer.swift
@@ -0,0 +1,38 @@
+//
+//  LocalImageRenderer.swift
+//  AsyncImageView
+//
+//  Created by Nacho Soto on 11/27/15.
+//  Copyright © 2015 Nacho Soto. All rights reserved.
+//
+
+import UIKit
+
+import ReactiveCocoa
+import Result
+
+public protocol LocalRenderDataType: RenderDataType {
+	var image: UIImage { get }
+}
+
+/// `RendererType` which loads images from the bundle.
+///
+/// Note that this Renderer will ignore `RenderDataType.size`.
+/// Consider chaining this with `ImageInflaterRenderer`.
+public final class LocalImageRenderer<T: LocalRenderDataType>: RendererType {
+	public init() {
+
+	}
+
+	public func renderImageWithData(data: T) -> SignalProducer<UIImage, NoError> {
+		return SignalProducer
+			.attempt { data.image.asResult }
+			.startOn(QueueScheduler())
+	}
+}
+
+extension UIImage {
+	private var asResult: Result<UIImage, NoError> {
+		return Result(self)
+	}
+}

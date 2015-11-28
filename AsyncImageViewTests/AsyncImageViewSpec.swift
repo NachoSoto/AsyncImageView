50283aa6e4fa5ff8fdb44b5a5416e959eee94f88
NachoSoto <ignaciosoto90@gmail.com>
2015-11-28 15:27:03 -0800

    AsyncImageViewSpec: testing that image is reset when updating data or frame

--- AsyncImageViewTests/AsyncImageViewSpec.swift
+++ AsyncImageViewTests/AsyncImageViewSpec.swift
@@ -12,94 +12,118 @@ import ReactiveCocoa
 
 @testable import AsyncImageView
 
-private typealias ViewType = AsyncImageView<TestRenderData, TestData, TestRenderer>
-
 class AsyncImageViewSpec: QuickSpec {
 	override func spec() {
 		describe("AsyncImageView") {
-			var view: ViewType!
-			var renderer: TestRenderer!
+			context("No placeholder") {
+				typealias ViewType = AsyncImageView<TestRenderData, TestData, TestRenderer>
 
-			beforeEach {
-				renderer = TestRenderer()
-				view = ViewType(
-					initialFrame: CGRectZero,
-					renderer: renderer,
-					imageCreationScheduler: ImmediateScheduler()
-				)
-			}
+				var view: ViewType!
+				var renderer: TestRenderer!
 
-			func verifyView() {
-				verifyImage(view.image, withSize: view.frame.size, data: view.data)
-			}
+				beforeEach {
+					renderer = TestRenderer()
+					view = ViewType(
+						initialFrame: CGRectZero,
+						renderer: renderer,
+						imageCreationScheduler: ImmediateScheduler()
+					)
+				}
 
-			it("has no image initially") {
-				expect(view.image).to(beNil())
-			}
+				func verifyView() {
+					verifyImage(view.image, withSize: view.frame.size, data: view.data)
+				}
 
-			context("Updating image") {
-				it("Does not render image if size is not set yet") {
-					view.data = .A
-
+				it("has no image initially") {
 					expect(view.image).to(beNil())
 				}
 
-				it("Updates image when setting data") {
-					view.frame.size = CGSize(width: 10, height: 10)
-					view.data = .A
+				context("Updating image") {
+					it("does not render image if size is not set yet") {
+						view.data = .A
 
-					verifyView()
+						expect(view.image).to(beNil())
+					}
+
+					it("updates image when setting data") {
+						view.frame.size = CGSize(width: 10, height: 10)
+						view.data = .A
+
+						verifyView()
+					}
+
+					it("updates image when updating data") {
+						view.frame.size = CGSize(width: 10, height: 10)
+						view.data = .A
+						view.data = .B
+
+						verifyView()
+					}
+
+					it("updates image when setting frame") {
+						view.data = .C
+						view.frame.size = CGSize(width: 10, height: 10)
+
+						verifyView()
+					}
+
+					it("updates image when updating frame") {
+						view.frame.size = CGSize(width: 10, height: 10)
+						view.data = .C
+
+						view.frame.size = CGSize(width: 15, height: 15)
+
+						verifyView()
+					}
+
+					it("resets image when updating data") {
+						view.frame.size = CGSize(width: 10, height: 10)
+						view.data = .C
+						verifyView()
+
+						view.data = .A
+
+						expect(view.image).to(beNil()) // image should be reset immediately
+						verifyView() // and updated when rendering finishes
+					}
+
+					it("resets image when updating frame") {
+						view.frame.size = CGSize(width: 10, height: 10)
+						view.data = .C
+						verifyView()
+
+						view.frame.size = CGSize(width: 15, height: 15)
+
+						expect(view.image).to(beNil()) // image should be reset immediately
+						verifyView() // and updated when rendering finishes
+					}
 				}
 
-				it("Updates image when updating data") {
-					view.frame.size = CGSize(width: 10, height: 10)
-					view.data = .A
-					view.data = .B
+				context("Not updating image if nothing changed") {
+					it("does not attempt to render anything is size is not ready") {
+						view.data = .A
+						view.frame.size = CGSize(width: 10, height: 0)
 
-					verifyView()
-				}
+						expect(view.image).toEventually(beNil())
+					}
 
-				it("Updates image when setting frame") {
-					view.data = .C
-					view.frame.size = CGSize(width: 10, height: 10)
+					it("only renders once if data does not change") {
+						view.data = .A
+						view.frame.size = CGSize(width: 10, height: 10)
+						view.data = .A
 
-					verifyView()
-				}
+						expect(view.image).toNotEventually(beNil())
+						expect(renderer.renderedImages.value) == [TestRenderData(data: view.data, size: view.frame.size)]
+					}
 
-				it("Updates image when updating frame") {
-					view.frame.size = CGSize(width: 10, height: 10)
-					view.data = .C
+					it("only renders once if size does not change") {
+						view.data = .A
+						view.frame = CGRect(origin: CGPointZero, size: CGSize(width: 10, height: 10))
+						view.frame = CGRect(origin: CGPoint(x: 1, y: 0), size: CGSize(width: 10, height: 10))
 
-					view.frame.size = CGSize(width: 15, height: 15)
-					
-					verifyView()
-				}
-			}
-
-			context("Not updating image if nothing changed") {
-				it("Does not attempt to render anything is size is not ready") {
-					view.data = .A
-					view.frame.size = CGSize(width: 10, height: 0)
-
-					expect(view.image).toEventually(beNil())
-				}
-
-				it("Only renders once if data does not change") {
-					view.data = .A
-					view.frame.size = CGSize(width: 10, height: 10)
-					view.data = .A
-
-					expect(view.image).toNotEventually(beNil())
-					expect(renderer.renderedImages.value) == [TestRenderData(data: view.data, size: view.frame.size)]
-				}
-
-				it("Only renders once if size does not change") {
-					view.data = .A
-					view.frame = CGRect(origin: CGPointZero, size: CGSize(width: 10, height: 10))
-					view.frame = CGRect(origin: CGPoint(x: 1, y: 0), size: CGSize(width: 10, height: 10))
-
-					expect(view.image).toNotEventually(beNil())
-					expect(renderer.renderedImages.value) == [TestRenderData(data: view.data, size: view.frame.size)]
+						expect(view.image).toNotEventually(beNil())
+						expect(renderer.renderedImages.value) == [TestRenderData(data: view.data, size: view.frame.size)]
+					}
 				}
 			}
 		}

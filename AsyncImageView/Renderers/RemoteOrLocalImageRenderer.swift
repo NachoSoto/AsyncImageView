b0d1f99e06568cd45769d76d4543175eb1ea8a91
NachoSoto <ignaciosoto90@gmail.com>
2017-02-17 12:29:38 -0800

    Created RemoteOrLocalImageRenderer to allow rendering locally or remotely, based on the input data

---
+++ AsyncImageView/Renderers/RemoteOrLocalImageRenderer.swift
@@ -0,0 +1,72 @@
+//
+//  RemoteOrLocalImageRenderer.swift
+//  AsyncImageView
+//
+//  Created by Nacho Soto on 2/17/17.
+//  Copyright © 2017 Nacho Soto. All rights reserved.
+//
+
+import UIKit
+
+import ReactiveSwift
+import Result
+
+public enum RemoteOrLocalRenderData<Local: LocalRenderDataType, Remote: RemoteRenderDataType>: RenderDataType {
+    case local(Local)
+    case remote(Remote)
+}
+
+/// `RendererType` which downloads images and/or loads images from the bundle.
+///
+/// - seealso: RemoteImageRenderer
+/// - seealso: LocalImageRenderer
+public final class RemoteOrLocalImageRenderer<Local: LocalRenderDataType, Remote: RemoteRenderDataType>: RendererType {
+    public typealias Data = RemoteOrLocalRenderData<Local, Remote>
+    
+    private let remoteRenderer: RemoteImageRenderer<Remote>
+    private let localRenderer: LocalImageRenderer<Local>
+    
+    public init(session: URLSession, scheduler: Scheduler = QueueScheduler()) {
+        self.remoteRenderer = RemoteImageRenderer(session: session)
+        self.localRenderer = LocalImageRenderer(scheduler: scheduler)
+    }
+    
+    public func renderImageWithData(_ data: Data) -> SignalProducer<UIImage, RemoteImageRendererError> {
+        switch data {
+        case let .remote(data):
+            return self.remoteRenderer
+                .renderImageWithData(data)
+            
+        case let .local(data):
+            return self.localRenderer
+                .renderImageWithData(data)
+                .promoteErrors(RemoteImageRendererError.self)
+        }
+    }
+}
+
+extension RemoteOrLocalRenderData {
+    public var hashValue: Int {
+        /// Not distinguishing between types, which can lead to collisions, but they're disambiguated in ==
+        switch self {
+        case let .local(data): return data.hashValue
+        case let .remote(data): return data.hashValue
+        }
+    }
+    
+    public static func == (lhs: RemoteOrLocalRenderData, rhs: RemoteOrLocalRenderData) -> Bool {
+        switch (lhs, rhs) {
+        case let (.local(lhs), .local(rhs)): return lhs == rhs
+        case let (.remote(lhs), .remote(rhs)): return lhs == rhs
+        
+        default: return false
+        }
+    }
+    
+    public var size: CGSize {
+        switch self {
+        case let .local(data): return data.size
+        case let .remote(data): return data.size
+        }
+    }
+}

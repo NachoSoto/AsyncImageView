18643093b44116e9096960434f53d4efeefb8982
NachoSoto <ignaciosoto90@gmail.com>
2017-02-24 12:08:26 -0800

    ContextRenderer: fixed indentation and docstrings

--- AsyncImageView/Renderers/ContextRenderer.swift
+++ AsyncImageView/Renderers/ContextRenderer.swift
@@ -12,35 +12,36 @@ import CoreGraphics
 /// `SynchronousRendererType` which generates a `UIImage` by rendering into a context.
 @available(iOS 10.0, tvOSApplicationExtension 10.0,  *)
 public final class ContextRenderer<Data: RenderDataType>: SynchronousRendererType {
-	public typealias Block = (_ context: CGContext, _ data: Data) -> ()
-
-	private let format: UIGraphicsImageRendererFormat
+    public typealias Block = (_ context: CGContext, _ data: Data) -> ()
+    
+    private let format: UIGraphicsImageRendererFormat
     private let imageSize: CGSize?
-	private let renderingBlock: Block
-
-	/// - opaque: A Boolean flag indicating whether the bitmap is opaque.
+    private let renderingBlock: Block
+    
+    /// - opaque: A Boolean flag indicating whether the bitmap is opaque.
+    /// If you know the bitmap is fully opaque, specify YES to ignore the
+    /// alpha channel and optimize the bitmap’s storage.
+    ///
     /// - imageSize: Optionally allows this Renderer to always create contexts of a constant size.
     ///              Useful for creating images that are going to be stretchable.
-	/// If you know the bitmap is fully opaque, specify YES to ignore the 
-	/// alpha channel and optimize the bitmap’s storage.
-	public init(scale: CGFloat, opaque: Bool, imageSize: CGSize? = nil, renderingBlock: @escaping Block) {
-		self.format = UIGraphicsImageRendererFormat()
-		self.format.opaque = opaque
-		self.format.scale = scale
-		self.imageSize = imageSize
-		self.renderingBlock = renderingBlock
-	}
-
-	public func renderImageWithData(_ data: Data) -> UIImage {
-		let renderer = UIGraphicsImageRenderer(
-			size: self.imageSize ?? data.size,
-			format: self.format
-		)
-
-		return renderer.image { context in
-			self.renderingBlock(UIGraphicsGetCurrentContext()!, data)
-		}
-	}
+    public init(scale: CGFloat, opaque: Bool, imageSize: CGSize? = nil, renderingBlock: @escaping Block) {
+        self.format = UIGraphicsImageRendererFormat()
+        self.format.opaque = opaque
+        self.format.scale = scale
+        self.imageSize = imageSize
+        self.renderingBlock = renderingBlock
+    }
+    
+    public func renderImageWithData(_ data: Data) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(
+            size: self.imageSize ?? data.size,
+            format: self.format
+        )
+        
+        return renderer.image { context in
+            self.renderingBlock(UIGraphicsGetCurrentContext()!, data)
+        }
+    }
 }
 
 /// `SynchronousRendererType` which generates a `UIImage` by rendering into a context.
@@ -53,10 +54,11 @@ public final class OldContextRenderer<Data: RenderDataType>: SynchronousRenderer
     private let renderingBlock: Block
     
     /// - opaque: A Boolean flag indicating whether the bitmap is opaque.
-    /// - imageSize: Optionally allows this Renderer to always create contexts of a constant size.
-    ///              Useful for creating images that are going to be stretchable.
     /// If you know the bitmap is fully opaque, specify YES to ignore the
     /// alpha channel and optimize the bitmap’s storage.
+    ///
+    /// - imageSize: Optionally allows this Renderer to always create contexts of a constant size.
+    ///              Useful for creating images that are going to be stretchable.
     public init(scale: CGFloat, opaque: Bool, imageSize: CGSize? = nil, renderingBlock: @escaping Block) {
         self.scale = scale
         self.opaque = opaque

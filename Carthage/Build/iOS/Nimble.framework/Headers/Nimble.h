03ff04a4334d94d932e8a3f0f9765730a43db5be
NachoSoto <ignaciosoto90@gmail.com>
2020-10-23 09:45:30 -0700

    Updated Carthage dependencies

--- Carthage/Build/iOS/Nimble.framework/Headers/Nimble.h
+++ Carthage/Build/iOS/Nimble.framework/Headers/Nimble.h
@@ -1,11 +1,11 @@
 #import <Foundation/Foundation.h>
-#import "NMBExceptionCapture.h"
-#import "NMBStringify.h"
-#import "DSL.h"
+#import <Nimble/NMBExceptionCapture.h>
+#import <Nimble/NMBStringify.h>
+#import <Nimble/DSL.h>
 
-#import "CwlMachBadInstructionHandler.h"
 #if TARGET_OS_OSX || TARGET_OS_IOS
-    #import "CwlCatchException.h"
+    #import <Nimble/CwlMachBadInstructionHandler.h>
+    #import <Nimble/CwlCatchException.h>
 #endif
 
 FOUNDATION_EXPORT double NimbleVersionNumber;

b4c52739e62dfae8fc1a4d823227cb00b6e4aea7
NachoSoto <ignaciosoto90@gmail.com>
2021-11-02 11:11:59 -0700

    Updated dependencies

---
+++ Carthage/Build/AsyncImageView.xcframework/tvos-arm64_x86_64-simulator/AsyncImageView.framework/Headers/AsyncImageView.h
@@ -0,0 +1,19 @@
+//
+//  AsyncImageView.h
+//  AsyncImageView
+//
+//  Created by Nacho Soto on 11/22/15.
+//  Copyright © 2015 Nacho Soto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncImageView.
+FOUNDATION_EXPORT double AsyncImageViewVersionNumber;
+
+//! Project version string for AsyncImageView.
+FOUNDATION_EXPORT const unsigned char AsyncImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncImageView/PublicHeader.h>
+
+

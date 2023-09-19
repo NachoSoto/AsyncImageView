7ce30709cb0ae414aad86c067ccfdccd9cc439b4
NachoSoto <ignaciosoto90@gmail.com>
2023-09-18 20:53:27 -0700

    Updated `Package.swift`

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "AsyncImageView",
-    platforms: [.iOS(.v13), .tvOS(.v13)],
+    platforms: [.iOS(.v13), .tvOS(.v13), .watchOS(.v9)],
     products: [
         .library(name: "AsyncImageView", targets: ["AsyncImageView"]),
     ],
     dependencies: [
-				.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "6.1.0")
+				.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "7.1.0")
     ],
     targets: [
         .target(
